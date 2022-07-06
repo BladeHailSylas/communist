fef4a2b462ed2d1d4ad678c5fbbbeeea9799c72b
BladeHailSylas <82759673+BladeHailSylas@users.noreply.github.com>
2022-07-06 15:03:47 +0900

    Something changed

--- Interactor.js
+++ Interactor.js
@@ -18,14 +18,15 @@ function docs(title, body, day) {
 
 function explore() {
     var docinfo = docs(writtenid('writingtitle'), writtenid('writtenbody'), nowday());
-    alert(docinfo);
+    console.log(docinfo);
+    return docinfo;
 }
 
 function nofirm() {
-    var v = confirm('작성 중인 글을 삭제하고 이전 페이지로 돌아갑니까?');
-    if (v == true) {
-        alert('글이 작성되지 않았습니다');
-        history.back();
+    var V = confirm('작성 중인 글을 삭제하고 이전 페이지로 돌아갑니까?');
+    if (V == true) {
+        alert('글이 작성되지 않았습니다.');
+        location.replace(document.referrer);
     }
     else { }
 }
@@ -33,7 +34,6 @@ function nofirm() {
 function yesfirm() {
     var V = confirm("작성을 완료하시겠습니까?");
     if (V == true) {
-        //데이터베이스를 여기서 연동, 글을 업로드
         alert("글이 업로드되었습니다.");
         location.replace(document.referrer);
     }
@@ -41,9 +41,8 @@ function yesfirm() {
 }
 
 function validAccess() {
-    var v = localStorage.getItem('token');
-    console.log(v);
-    if (v != 'true') {
+    /*var V = localStorage.getItem('token');*/
+    if (localStorage.getItem('token') != 'true') {
         alert("유효하지 않은 접근입니다.");
         history.back();
     }
